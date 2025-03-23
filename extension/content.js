8007a17b381e0f284e7223fdb8a08adcd7e2c793
codingdestro <codingdestro@gmail.com>
2025-03-23 15:34:12 +0530

    delete: the extension for plasmo integratoin

--- extension/content.js
+++
@@ -1,3 +0,0 @@
-// This content script can be used to interact with web pages
-// Currently empty as the extension doesn't need page-specific functionality
-console.log('Coverly content script loaded'); 
