967a7ddddcff74d9b0955efb38f79b83d55d8028
codingdestro <codingdestro@gmail.com>
2025-04-18 21:26:42 +0530

    update: login endpoint

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,12 +1,30 @@
-import { login, register, logout, authenticate } from "../../controllers/user";
+import {
+  login,
+  register,
+  logout,
+  authenticate,
+  handleLogin,
+} from "../../controllers/user";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/login", login, (req, res) => res.redirect("/dashboard"));
-router.post("/ext/login", login, (req, res) => {
-  res.json({ token: req.body.token }).status(200);
-});
+router.post("/login", login);
+router.post(
+  "/ext/login",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const { email, password } = req.body;
+      if (!email && !password)
+        res.json({ error: "no credentianls" }).status(401);
+      const token = await handleLogin(email, password);
+      res.json({ token: req.body.token }).status(200);
+      res.json({ token }).status(200);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+);
 router.post("/logout", logout);
 router.post("/register", register);
 router.post("/authenticate", authenticate);
